b5bf3fc0c9f43ac6d683c44deea26c5c12066352
Yatesh Sharma <yateshsharma485@gmail.com>
2026-07-13 14:27:14 +0530

    Change scheduled scraping time to 08:51

--- smap_web/stocks/updater.py
+++ smap_web/stocks/updater.py
@@ -26,8 +26,8 @@ def start_pipeline_scheduler():
         id = 'daily_market_scraping',
         trigger=CronTrigger(
             day_of_week='mon-fri',
-            hour=14,
-            minute=26
+            hour=8,
+            minute=51
         )
     )
     
