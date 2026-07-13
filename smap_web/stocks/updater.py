09bdc07eb2259eeac8836b34f1004ddf76aef25f
Yatesh Sharma <yateshsharma485@gmail.com>
2026-07-13 14:59:56 +0530

    updated date

--- smap_web/stocks/updater.py
+++ smap_web/stocks/updater.py
@@ -26,8 +26,8 @@ def start_pipeline_scheduler():
         id = 'daily_market_scraping',
         trigger=CronTrigger(
             day_of_week='mon-fri',
-            hour=12,
-            minute=48
+            hour=16,
+            minute=30
         )
     )
     
