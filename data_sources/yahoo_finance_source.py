52b06bf3e6e611f6cece49bd987dec87f5d9b515
Yatesh Sharma <yateshsharma485@gmail.com>
2026-07-14 18:42:26 +0530

    fixed too many requests error

--- data_sources/yahoo_finance_source.py
+++ data_sources/yahoo_finance_source.py
@@ -3,6 +3,16 @@ import pandas as pd
 from datetime import datetime, timedelta
 import yfinance as yf
 
+import time
+import random    
+import requests
+
+# custom session to spoof real webbrowser (to avoid too many requests error)
+yfinance_session = requests.Session()
+yfinance_session.headers.update({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+})
+
 class YahooFinanceSource(BaseSource):
     
     def connect(self):
@@ -26,10 +36,11 @@ class YahooFinanceSource(BaseSource):
             
             # yfinance sets the Date as the index. We need it as a normal column.
             df = df.reset_index()
-            
             df['symbol'] = symbol
-            
             all_stock_data.append(df)
+            
+            # Force Python to pause for 2-4 seconds before the next symbol  
+            time.sleep(random.uniform(2, 4))
             
         if all_stock_data:
             final_df = pd.concat(all_stock_data, ignore_index=True)
